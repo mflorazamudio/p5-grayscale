eae147a7335832784d3d3ffbd92ce7a48a77be41
Maria Flora Zamudio <mflorazamudio@wlapcs.org>
2018-01-11 00:49:58 +0000

    built grayscale demo

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
 function setup(){
     createCanvas(600,400);
+    background(255);
+    noStroke();
+    num = 0;
+    frameRate(10);
 }
 
 function draw(){
-    
+    if(num < 256){
+        fill(num);
+        ellipse(width/2, height/2, 250, 250);
+        
+        
+        fill("#008080");
+        textSize(25);
+        textAlign(CENTER, CENTER);
+        text(num, width/2, height/2);
+        
+        num = num + 1;
+    }
 }
